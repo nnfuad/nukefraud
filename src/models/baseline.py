ec65035ee17afb146ce09258095395f8c1c011c8
nnfuad <nnfuad01@gmail.com>
2026-03-01 16:32:01 +0600

    updated trainer.py with optimized threshold

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -6,7 +6,8 @@ def get_logistic_model():
     return LogisticRegression(
         class_weight="balanced",
         max_iter=1000,
-        n_jobs=-1,
+        solver="lbfgs", # Use 'lbfgs' for better convergence on larger datasets, lbgs allows for L2 regularization which can help with imbalanced data
+        # n_jobs=-1 is removed for In modern sklearn versions, LogisticRegression does not support n_jobs, as it is not parallelized.
     )
 
 
--- src/models/trainer.py
+++ src/models/trainer.py
@@ -1,8 +1,5 @@
-# src/models/trainer.py
-
 import joblib
 from pathlib import Path
-
 from ..data_loader import load_raw_data
 from ..preprocessing import split_data, scale_features
 from ..evaluation import evaluate_model
@@ -20,19 +17,44 @@ def train_baseline():
     model.fit(X_train_scaled, y_train)
 
     y_proba = model.predict_proba(X_test_scaled)[:, 1]
+    
+    from ..thresholding import find_best_threshold
+    
+    best_threshold, best_cost = find_best_threshold(y_test, y_proba)
 
-    results = evaluate_model(y_test, y_proba)
+    print("\n=== Cost-Based Optimization ===")
+    print("Best Threshold:", best_threshold)
+    print("Lowest Expected Cost:", best_cost)
 
-    print("=== Evaluation ===")
-    print("ROC-AUC:", results["ROC_AUC"])
-    print("PR-AUC:", results["PR_AUC"])
-    print(results["Classification_Report"])
-    print("Confusion Matrix:\n", results["Confusion_Matrix"])
+    print("\n=== Evaluation @ Optimized Threshold ===")
+    optimized_results = evaluate_model(
+        y_test, y_proba, threshold=best_threshold
+    )
+    print("ROC-AUC:", optimized_results["ROC_AUC"])
+    print("PR-AUC:", optimized_results["PR_AUC"])
+    print(optimized_results["Classification_Report"])
+    print("Confusion Matrix:\n", optimized_results["Confusion_Matrix"])
+    
+    # results = evaluate_model(y_test, y_proba)
+    # # Default evaluation (threshold = 0.5)
+    # print("\n=== Evaluation @ Default Threshold (0.5) ===")
+    # default_results = evaluate_model(y_test, y_proba, threshold=0.5)
+    # print("ROC-AUC:", default_results["ROC_AUC"])
+    # print("PR-AUC:", default_results["PR_AUC"])
+    # print(default_results["Classification_Report"])
+    # print("Confusion Matrix:\n", default_results["Confusion_Matrix"])
+    
+    # print("=== Evaluation ===")
+    # print("ROC-AUC:", results["ROC_AUC"])
+    # print("PR-AUC:", results["PR_AUC"])
+    # print(results["Classification_Report"])
+    # print("Confusion Matrix:\n", results["Confusion_Matrix"])
 
     Path(MODEL_DIR).mkdir(exist_ok=True)
 
     joblib.dump(model, f"{MODEL_DIR}/logistic_model.joblib")
     joblib.dump(scaler, f"{MODEL_DIR}/scaler.joblib")
+    print("\nModel and scaler saved successfully.")
 
 
 if __name__ == "__main__":
